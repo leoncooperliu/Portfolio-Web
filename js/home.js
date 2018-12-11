0ce0f86901242a8dc3a397f1fda30bb08edc8631
Leon Liu <leoncooper@outlook.com>
2018-12-10 23:20:08 -0500

    2.0

--- js/home.js
+++ js/home.js
@@ -1,24 +1,84 @@
 const FILMS = {
-    page: ["film-near.html", "film-chasing.html", "film-gala.html"],
-    location: ["mp4/home-near1.mp4", "mp4/home-shadow.mp4", "mp4/home-gala.mp4"],
-    name: ["View Short Film", "View Feature Film", "View Trailer"],
-    poster: ["img/home-film-near.jpg", "img/home-film-shadow.jpg", "img/home-film-gala.jpg"],
-    type: ["mp4", "mp4", "mp4"]
+    page: ["film-near.html",
+           "film-chasing.html",
+           "film-gala.html"],
+    location: ["mp4/home-near1.mp4",
+               "mp4/home-shadow.mp4",
+               "mp4/home-gala.mp4"],
+    name: ["View Short Film",
+           "View Feature Film",
+           "View Trailer"],
+    poster: ["img/home-film-near.jpg",
+             "img/home-film-shadow.jpg",
+             "img/home-film-gala.jpg"],
+    type: ["mp4",
+           "mp4",
+           "mp4"]
 }
 
 const PHOTOS = {
-    page: ["photo-dancer.html", "photo-kathy.html", "photo-long.html"],
-    location: ["img/home-photo-dancer.jpg", "img/home-photo-kathy.jpg", "img/home-photo-long.jpg"],
-    name: ["View Portraiture", "View Portraiture", "View Landscape"],
-    type: ["img", "img", "img"]
+    page: ["photo-dancer.html",
+           "photo-kathy.html",
+           "photo-long.html"],
+    location: ["img/home-photo-dancer.jpg",
+               "img/home-photo-kathy.jpg",
+               "img/home-photo-long.jpg"],
+    name: ["View Portraiture",
+           "View Portraiture",
+           "View Landscape"],
+    type: ["img",
+           "img",
+           "img"]
 }
 
 const RECENTS = {
-    page: ["photo-dancer.html", "film-near.html", "photo-kathy.html", "film-chasing.html", "photo-long.html", "film-gala.html"],
-    location: ["img/home-photo-dancer.jpg", "mp4/home-near1.mp4", "img/home-photo-kathy.jpg", "mp4/home-shadow.mp4", "img/home-photo-long.jpg", "mp4/home-gala.mp4"],
-    name: ["View Portraiture", "View Short Film", "View Portraiture", "View Feature Film", "View Landscape", "View Trailer"],
-    poster: ["", "img/home-film-near.jpg", "", "img/home-film-shadow.jpg", "", "img/home-film-gala.jpg"],
-    type: ["img", "mp4", "img", "mp4", "img", "mp4"]
+    page: ["photo-dancer.html",
+           "film-near.html",
+           "photo-kathy.html",
+           "film-chasing.html",
+           "photo-long.html",
+           "film-gala.html"],
+    location: ["img/home-photo-dancer.jpg",
+               "mp4/home-near1.mp4",
+               "img/home-photo-kathy.jpg",
+               "mp4/home-shadow.mp4",
+               "img/home-photo-long.jpg",
+               "mp4/home-gala.mp4"],
+    name: ["View Portraiture",
+           "View Short Film",
+           "View Portraiture",
+           "View Feature Film",
+           "View Landscape",
+           "View Trailer"],
+    poster: ["",
+             "img/home-film-near.jpg",
+             "",
+             "img/home-film-shadow.jpg",
+             "",
+             "img/home-film-gala.jpg"],
+    type: ["img",
+           "mp4",
+           "img",
+           "mp4",
+           "img",
+           "mp4"]
+}
+
+const BLOGS = {
+    page: ["https://nyu-web.github.io/homework-2-leoncooperliu/",
+           "https://nyu-web.github.io/homework-3-leoncooperliu/",
+           "https://nyu-web.github.io/homework-5-leoncooperliu/",
+           "https://nyu-web.github.io/homework-8-leoncooperliu/"],
+    location: ["img/blogs/hw-2.jpg",
+               "img/blogs/hw-3.jpg",
+               "img/blogs/hw-5.jpg",
+               "img/blogs/hw-8.jpg"],
+    name: ["View on GitHub",
+           "View on GitHub",
+           "View on GitHub",
+           "View on GitHub"],
+    poster: ["", "", "", ""],
+    type: ["img", "img", "img", "img"]
 }
 
 console.log("Hi");
@@ -29,6 +89,7 @@ const films = document.querySelector('#nav-film');
 const recent = document.querySelector('#nav-recent');
 const photos = document.querySelector('#nav-photo');
 const designs = document.querySelector('#nav-design');
+const blogs = document.querySelector('#nav-blogs');
 
 const showProjects = (inputArray) => {
     allProject.innerHTML = "";
@@ -52,14 +113,14 @@ const showProjects = (inputArray) => {
 
             a2.setAttribute('href', inputArray["page"][i]);
             title.innerHTML = inputArray["name"][i];
-            title.classList.add('nova-semibold');
+            title.classList.add('font--nova-semibold');
             a2.appendChild(title);
             console.log(title);
 
             inDiv.appendChild(a2);
-            inDiv.classList.add('card-text');
+            inDiv.classList.add('works__card__text');
 
-            outDiv.classList.add('card', 'animated', 'fadeInUp');
+            outDiv.classList.add('works__card', 'animated', 'fadeInUp');
             outDiv.appendChild(a1);
             outDiv.appendChild(inDiv);
             allProject.appendChild(outDiv);
@@ -74,47 +135,55 @@ const showProjects = (inputArray) => {
 
             a2.setAttribute('href', inputArray["page"][i]);
             title.innerHTML = inputArray["name"][i];
-            title.classList.add('nova-semibold');
+            title.classList.add('font--nova-semibold');
             a2.appendChild(title);
             console.log(title);
 
             inDiv.appendChild(a2);
-            inDiv.classList.add('card-text');
+            inDiv.classList.add('works__card__text');
 
-            outDiv.classList.add('card', 'animated', 'fadeInUp');
+            outDiv.classList.add('works__card', 'animated', 'fadeInUp');
             outDiv.appendChild(a1);
             outDiv.appendChild(inDiv);
             allProject.appendChild(outDiv);
             console.log('sucess-img');
         }
-
     }
 }
 
 const showFilmProjects = (event) => {
     showProjects(FILMS);
-    films.classList.add('ls-active');
-    designs.classList.remove('ls-active');
-    photos.classList.remove('ls-active');
-    recent.classList.remove('ls-active');
+    films.classList.add('btn--active');
+    blogs.classList.remove('btn--active');
+    photos.classList.remove('btn--active');
+    recent.classList.remove('btn--active');
 }
 
 const showPhotoProjects = (event) => {
     showProjects(PHOTOS);
-    photos.classList.add('ls-active');
-    designs.classList.remove('ls-active');
-    films.classList.remove('ls-active');
-    recent.classList.remove('ls-active');
+    photos.classList.add('btn--active');
+    blogs.classList.remove('btn--active');
+    films.classList.remove('btn--active');
+    recent.classList.remove('btn--active');
 }
 
 const showRecentProjects = (event) => {
     showProjects(RECENTS);
-    recent.classList.add('ls-active');
-    designs.classList.remove('ls-active');
-    photos.classList.remove('ls-active');
-    films.classList.remove('ls-active');
+    recent.classList.add('btn--active');
+    blogs.classList.remove('btn--active');
+    photos.classList.remove('btn--active');
+    films.classList.remove('btn--active');
+}
+
+const showBlogs = (event) => {
+    showProjects(BLOGS);
+    blogs.classList.add('btn--active');
+    recent.classList.remove('btn--active');
+    photos.classList.remove('btn--active');
+    films.classList.remove('btn--active');
 }
 
 films.addEventListener('click', showFilmProjects);
 photos.addEventListener('click', showPhotoProjects);
 recent.addEventListener('click', showRecentProjects);
+blogs.addEventListener('click', showBlogs);
